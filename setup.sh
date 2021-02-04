e57a097f1de3c33447329563aa2c3a3144ceb369
Alam Sanchez <asancheztorres1905@gmail.com>
2021-02-03 23:12:05 -0600

    Update heroku config

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ headless = true
 port = $PORT
 enableCORS = false
 " > ~/.streamlit/config.toml
+wget -P ./config https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
